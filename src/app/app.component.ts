c07400e9c1b1d0d99bb1f7233ecc903e6840ea93
Njarasoa Tommy <njarasoatommy@gmail.com>
2026-04-08 04:15:52 +0300

    Implement the main component(app).

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { AboutComponent } from './components/about/about.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { ContactComponent } from './components/contact/contact.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    NavbarComponent,
+    HeroComponent,
+    AboutComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    ExperienceComponent,
+    ContactComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'frontend';
+  title = "Njarasoa's Portfolio";
 }
